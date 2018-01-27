0630261b789a39cefbc59f15121af760670c9c71
Patrick <patrick.chao.14@gmail.com>
2018-01-26 20:57:32 -0800

    Create User Model and Postgres database

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  attr_reader :password
+
+  before_validation :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user.nil? || user.is_password?(password)
+    nil
+  end
+
+end
