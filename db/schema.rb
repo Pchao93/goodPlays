14f40c18c6c64eba2d2d370867abe095f4e05a44
Patrick <patrick.chao.14@gmail.com>
2018-02-08 15:42:40 -0800

    Add count to collections to increase performance of sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207234607) do
+ActiveRecord::Schema.define(version: 20180208204612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180207234607) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "count"
     t.index ["name", "user_id"], name: "index_collections_on_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_collections_on_user_id"
   end
